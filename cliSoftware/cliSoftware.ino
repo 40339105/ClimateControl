d2cd956b5dbed2e353fe6788f46c200ee229a87d
40339105 <36671490+40339105@users.noreply.github.com>
2019-05-30 16:11:46 +0100

    Update cliSoftware.ino

--- cliSoftware/cliSoftware.ino
+++ cliSoftware/cliSoftware.ino
@@ -3,11 +3,11 @@ int sensorPin = 0; //the analog pin the TMP36's Vout (sense) pin is connected to
                         //the resolution is 10 mV / degree centigrade with a
                         //500 mV offset to allow for negative temperatures
 int fanPin = 3; //Pin used to control the fan
-int heaterPin = 10; //Pin used to control the heater
+int heaterPin = 9; //Pin used to control the heater
 
 
 //PID Setup
-double Input, Output, Setpoint = 24.0;
+double Input, Output, Setpoint = 300;
 double Kp=0, Ki = 0, Kd=0;
 PID myPid(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
@@ -23,21 +23,19 @@ void setup() //Serial connection for debuging and sat fan pin's output
 }
  
 void loop()                     // run over and over again
-{
- Input = getTemp()/2;
+{ 
+ Input = getTemp();
+ Serial.println(Input);
  myPid.Compute();
  heater();
- delay(1000);
- Serial.println(getTemp());
+ //Serial.println(getTemp())
 }
 
 void heater(){
   //Spin the fan and turn the heater off based on the temperature
   analogWrite(heaterPin, Output);
-  if(Input > Setpoint){
-    analogWrite(fanPin, 0);
-  }
-  delay(50);
+  analogWrite(fanPin, 255);
+  delay(5000);
 }
 
 float getTemp(){
